6b497c7e8897b395624483fe0553813466468da4
Michael C <159742740+miconnors812@users.noreply.github.com>
2024-12-11 22:56:36 -0500

    updating files
    user functions has been changed, and I added another response to the csv

--- UserFunctions.py
+++ UserFunctions.py
@@ -6,13 +6,16 @@ import re           # for regular expressions
 # Helper method to find out if a worker has inputted their information into the system
 def worker_exists(worker,database):
     exists = 0 # 0 is the base case and means that the worker does not have an entry in the database
-    match = re.i("all", worker) # check if the user inputted "all" case insensitively
-    if match == None:
-        for i in range(1,len(database[2])):
-            if (worker == database[2][i]):
-                exists = 1
-    else:
-        exists = 2; # if "all" is inputted as the worker name, then there is a special case for the function
+    #match = re.I(r"all", worker) # possibly use case-insensitive search?
+    if (worker == "all"):   # check if the user inputted "all"
+        exists = 2; # if "all" is inputted as the worker name, then there is a special case for the function to perform
+    else: 
+        # loop through all workers and see if any name matches the input
+        for i in range(1,len(database)):       # go through each response, excluding the format example 
+                print(i)
+                if (worker == database[i][2]): # database[2] is the column that holds all worker names in the spreadsheet
+                    exists = 1
+    #print(exists)
     return exists
 
 
@@ -26,7 +29,7 @@ def pay(worker,database):
             # Determine Worker's job position
             job = ""
             for i in range(1,len(database)):
-                if (database[i][2] == worker & database[i][1] != job):
+                if (database[i][2] == worker) & (database[i][1] != job):
                     job = database[i][1]
             if (job == "Worker"):
                 wage = 15        # $15 an hour for workers
@@ -37,20 +40,23 @@ def pay(worker,database):
 
             # Next, determine their hours
             #time = hours(worker,database)
-            time = 5
+            
+            time = 5 #TEST VALUEEE
 
             # Finally, calculate their earnings
             earnings = time * wage
 
         else:
             # "all" has been inputted, so add up every worker's pay.
-            workers = [database[2]] # every worker
             # However, that includes duplicates for workers who inputted multiple times,
-            # So we must weed out those names that appear multiple times.
-            for i in range(1,len(workers)):
-                for j in range(1,len(workers)):
-                    if (workers[i] == workers[j]):
-                        workers = workers[i:] + workers[:j+1]
+            # So we must also weed out those names that appear multiple times.
+            workers = []
+            for i in range(1,len(database)):
+                workers.append(database[i][2])  # preemptively add their name to the end
+                for j in range(len(workers)-1):   # check every name but the most recent
+                    if (workers[j] == workers[-1]): # has this name already been put in the array of workers?
+                        workers.pop()                  # if so, remove them
+
             print(workers)
                     #if (database[2][i] == database[2][j]):
 

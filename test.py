85012516d8c5d5be03c68ba632e0b6a46ac4cb28
evtilton <evtilton@ursinus.edu>
2024-12-12 22:40:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import unittest
+import UserFunctions
 
-class TestSum(unittest.TestCase):
+class TestFunctions(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+        UserFunctions.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
 if __name__ == '__main__':
     unittest.main()
